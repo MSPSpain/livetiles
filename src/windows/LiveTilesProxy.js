8552e094e1c37c100bf6cc2bee9bea43d4e8061d
ckgrafico <CBR600rr>
2015-04-06 11:43:01 +0200

    Add options

--- src/windows/LiveTilesProxy.js
+++ src/windows/LiveTilesProxy.js
@@ -5,39 +5,46 @@ function createTile(options) {
 }
 
 Windows.UI.Notifications.TileUpdateManager.createTileUpdaterForApplication().enableNotificationQueue(true);
-getTileData();
 
 
 module.exports = {
 	updateAppTile: function(win, lose, options) {
 		var notifications = Windows.UI.Notifications;
 
-		var template = notifications.TileTemplateType.tileWideText02;
-		var tileXml = notifications.TileUpdateManager.getTemplateContent(template);
+		// https://msdn.microsoft.com/en-us/library/windows/apps/windows.ui.notifications.tiletemplatetype.aspx
+		var template = notifications.TileTemplateType.tileWideImageAndText01;
+	    var tileXml = notifications.TileUpdateManager.getTemplateContent(template);
 
-		var tileTextAttributes = tileXml.getElementsByTagName("title");
-		tileTextAttributes[0].appendChild(tileXml.createTextNode('Titulo')); // Title
+	    var tileTextAttributes = tileXml.getElementsByTagName("text");
+	    tileTextAttributes[0].appendChild(tileXml.createTextNode(options.title)); // Text
 
-		var tileTextAttributes = tileXml.getElementsByTagName("text");
-		tileTextAttributes[0].appendChild(tileXml.createTextNode('Prueba de texto')); // Text
+	    var tileImageAttributes = tileXml.getElementsByTagName("image");
+	    tileImageAttributes[0].setAttribute("src", options.image); // Large image
+	    tileImageAttributes[0].setAttribute("alt", "red graphic");
 
-		var squareTemplate = notifications.TileTemplateType.tileSquareText04; 
-		var squareTileXml = notifications.TileUpdateManager.getTemplateContent(squareTemplate);
+	    var squareTemplate = notifications.TileTemplateType.tileSquareText04; 
+	    var squareTileXml = notifications.TileUpdateManager.getTemplateContent(squareTemplate);
 
-		var squareTileTextAttributes = squareTileXml.getElementsByTagName("title");
-		squareTileTextAttributes[0].appendChild(squareTileXml.createTextNode('Titulo')); // Text
+	    var squareTileTextAttributes = squareTileXml.getElementsByTagName("text");
+	    squareTileTextAttributes[0].appendChild(squareTileXml.createTextNode(options.title)); // Text
 
-		var squareTileTextAttributes = squareTileXml.getElementsByTagName("text");
-		squareTileTextAttributes[0].appendChild(squareTileXml.createTextNode('Prueba de texto')); // Text
+	    var node = tileXml.importNode(squareTileXml.getElementsByTagName("binding").item(0), true);
+	    tileXml.getElementsByTagName("visual").item(0).appendChild(node);
 
-		var node = tileXml.importNode(squareTileXml.getElementsByTagName("binding").item(0), true);
-		tileXml.getElementsByTagName("visual").item(0).appendChild(node);
+	    var tileNotification = new notifications.TileNotification(tileXml);
 
-		var tileNotification = new notifications.TileNotification(tileXml);
+	    tileNotification.tag = "Tilebox";
+	    notifications.TileUpdateManager.createTileUpdaterForApplication().update(tileNotification);
 
-		tileNotification.tag = "Tilebox";
-		notifications.TileUpdateManager.createTileUpdaterForApplication().update(tileNotification);
-
+		// TODO more options
+		/*
+			#title: 'title', 
+		    #image:'Images/appbar.next.rest.png', 
+		    count: 5, 
+		    backTitle: 'Back title', 
+		    backContent:'Back side', 
+		    backImage : 'Images/appbar.close.rest.png'
+		*/
 	},
 
 	createSecondaryTile: function(win, lose, options) {
